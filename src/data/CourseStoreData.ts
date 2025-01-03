7027c1e25ea10856b006cba14a50e58526355576
Leijie Wang <leijiew@cs.washington.edu>
2025-01-02 17:24:58 -0800

    Update the website from autumn 2024 to winter 2025.

--- src/data/CourseStoreData.ts
+++ src/data/CourseStoreData.ts
@@ -2,9 +2,9 @@ import { CourseStoreData } from "@/types/CourseStore";
 
 export const courseStoreData: CourseStoreData = {
   // Link to course Canvas. No trailing slash.
-  linkCanvas: "https://canvas.uw.edu/courses/1746586",
+  linkCanvas: "https://canvas.uw.edu/courses/1779838",
   // Link to course GitHub.
-  linkGitHub: "https://github.com/uwcse440/web-cse440-24au",
+  linkGitHub: "https://github.com/uwcse440/web-cse440-wi25",
   // Link to university syllabus policies and guidelines.
   linkUniversitySyllabusGuidelines:
     "https://registrar.washington.edu/curriculum/syllabus-guidelines/",
